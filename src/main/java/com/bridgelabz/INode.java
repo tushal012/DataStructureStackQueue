96f2fc97fdcfbb8e209463d489a5381121dabd57
tushal kumar <tushalkashyap11@gmail.com>
2021-06-20 07:43:34 -0700

    Added: to create a stack of 56->30->70

---
+++ src/main/java/com/bridgelabz/INode.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public interface INode<K> {
+    K getKey();
+
+    void setKey(K key);
+
+    INode getNext();
+
+    void setNext(INode next);
+}
+
---
+++ src/test/java/com/bridgelabz/MyStackTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyStackTest {
+    @Test
+    public void given3Numbers_WhenAddedToStack_ShouldHaveLastAddedNode() {
+        MyStack myStack = new MyStack();
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+        INode peak = myStack.peak();
+        myStack.printStack();
+        Assertions.assertEquals(myThirdNode, peak);
+    }
+}
